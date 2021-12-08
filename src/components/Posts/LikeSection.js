6fb88f330dd5a96d5eb12919aca673ba90263265
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2021-12-07 19:14:37 -0600

    heart like buttons working now

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -2,24 +2,21 @@
 // Use a piece of data coming in through props to display the correct number of likes.
 // You will also add an onClick handler that utilizes `likePost` to increase the count of likes.
 // (As a stretch goal, you might want to prevent your user from "liking" the same post more than once.)
-import React, { useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 
 const LikeSection = props => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
   const { likePost, numberOfLikes } = props; 
-  const [numberLikes, setNumberLikes] = useState(likePost); 
+  //const [numberLikes, setNumberLikes] = useState(likePost); 
   return (
     <div>
       <div
         className='like-section'
         key='likes-icons-container'>
-        <div className='like-section-wrapper'>
-          <FontAwesomeIcon icon={faHeart} 
-          onClick={() => {
-            setNumberLikes(numberLikes + 1); 
-          }}/>
+        <div className='like-section-wrapper' onClick={likePost}>
+          <FontAwesomeIcon icon={faHeart} />
         </div>
         <div className='like-section-wrapper'>
           <FontAwesomeIcon icon={faComment} />
